5ffe03c9a3dd84c90fc135da050002732c5e9f86
Asif Istiaque <asifistiaque.ai@gmail.com>
2024-12-04 18:10:49 +0600

    s

--- user-controllers/store/getStore.controller.ts
+++ user-controllers/store/getStore.controller.ts
@@ -1,13 +1,33 @@
 import { Response } from 'express';
 import Store from '../../models/store/store.model.js';
+import Shop from '../../models/shop/shop.model.js';
 
 const getStore = async (req: any, res: any): Promise<Response> => {
 	try {
-		const data = await Store.findOne({
+		let data = await Store.findOne({
 			shop: req.shop || '0001',
 		});
 		if (!data) {
-			return res.status(404).json({ message: 'Store not found' });
+			const shop = await Shop.findOne({ id: req.shop });
+			const newStore = new Store({
+				shop: req.shop,
+				basic: {
+					name: shop?.name,
+					logo: shop?.logo,
+					phone: shop?.phone,
+					email: shop?.email,
+				},
+				socials: {
+					facebook: shop?.facebook,
+					twitter: shop?.twitter,
+					instagram: shop?.instagram,
+					linkedin: shop?.linkedin,
+					youtube: shop?.youtube,
+				},
+				isActive: true,
+			});
+			const saved = await newStore.save();
+			data = await Store.findOne({ _id: saved._id }).populate('shop');
 		}
 		return res.status(200).json(data);
 	} catch (e: any) {
