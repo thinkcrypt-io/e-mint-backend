596ca6452e8e3e9d047dfa473e118c6aba8ea49c
Asif Istiaque <asifistiaque.ai@gmail.com>
2024-12-24 14:44:45 +0600

    s

--- routes/content.route.ts
+++ routes/content.route.ts
@@ -73,7 +73,7 @@ router.get('/pulse', protect, getThemeContent(Pulse));
 router.put('/pulse', protect, updateThemeContent(Pulse));
 
 router.post('/product/pulse', protect, addProductListToTheme(Pulse));
-router.delete('/product/pulse/:id', protect, deleteProductListFromTheme(Pulse));
-router.put('/product/pulse/:id', protect, updateProductListInTheme(Pulse));
+// router.delete('/product/pulse/:id', protect, deleteProductListFromTheme(Pulse));
+// router.put('/product/pulse/:id', protect, updateProductListInTheme(Pulse));
 
 export default router;
