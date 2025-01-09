147d355338de2f8ad3398961064ab767177251c2
DevSiamThinkcrypt <siam.thinkcrypt@gmail.com>
2025-01-09 18:25:16 +0600

    checkout css

--- models/store/pulse.model.ts
+++ models/store/pulse.model.ts
@@ -336,7 +336,7 @@ const schema = new Schema<any>(
 				// textColor: colors?.fg,
 				banner: {
 					imgSrc: getImage(),
-					text: getString('Enter your title here'),
+					text: getString('About'),
 				},
 				mission: {
 					label: getString('Our Mission'),
@@ -558,6 +558,51 @@ const schema = new Schema<any>(
 
 				btnFontSize: getNumber(16),
 				btnFontWeight: getNumber(600),
+			},
+			checkoutCss: {
+				bgColor: colors?.bg,
+				fgColor: colors?.fg,
+				cardBg: colors?.bg,
+				cardFg: colors?.fg,
+				headingSizeBase: getNumber(18),
+				headingSizeBg: getNumber(24),
+				headingWeight: getNumber(600),
+
+				cardTitleSizeBase: getNumber(16),
+				cardTitleSizeBg: getNumber(20),
+				cardTitleWeight: getNumber(600),
+
+				labelSize: getNumber(16),
+				labelWeight: getNumber(500),
+
+				inputBorder: colors?.fg,
+
+				nameSizeBase: getNumber(12),
+				nameSizeBg: getNumber(18),
+				nameWeight: getNumber(500),
+				nameColor: colors?.fg,
+
+				qtySizeBase: getNumber(12),
+				qtySizeBg: getNumber(18),
+				qtyWeight: getNumber(400),
+				qtyColor: colors?.fg,
+
+				totalSizeBase: getNumber(12),
+				totalSizeBg: getNumber(18),
+				totalWeight: getNumber(400),
+				totalColor: colors?.fg,
+
+				summarySizeBase: getNumber(12),
+				summarySizeBg: getNumber(18),
+				summaryWeight: getNumber(600),
+				summaryColor: colors?.fg,
+
+				btnBg: colors?.bg,
+				btnFg: colors?.fg,
+				btnHoverBg: colors?.bg,
+				btnHoverFg: colors?.fg,
+				btnSize: getNumber(16),
+				btnWeight: getNumber(500),
 			},
 		},
 
