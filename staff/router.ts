6e1c34ddcd8f6f8345f2c98eea3fdb7bbe0d32ad
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-01-21 16:15:50 +0600

    s

--- staff/router.ts
+++ staff/router.ts
@@ -3,11 +3,13 @@ import express from 'express';
 import authRoute from './staffAuthRoute.js';
 import productRoute from './products.staff.route.js';
 import categoriesRoute from './controllers/categories.staff.route.js';
+import orderRoute from './order.staff.route.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoute);
 router.use('/products', productRoute);
 router.use('/categories', categoriesRoute);
+router.use('/orders', orderRoute);
 
 export default router;
