5e72a2c743f135af752cf9a755cca20d621f6bbb
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-02-02 14:37:50 +0600

    s

--- models/store/store.model.ts
+++ models/store/store.model.ts
@@ -238,7 +238,11 @@ const schema = new Schema<any>(
 							type: String,
 							default: 'Button',
 						},
-						image: String,
+						image: {
+							type: String,
+							default:
+								'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1704931200&semt=ais',
+						},
 						href: {
 							type: String,
 							default: '/',
