80652324e7127e72b16157f5fbc6fc401e6edeed
arefin.aareef <arefin.aareef@gmail.com>
2025-03-13 14:03:06 +0600

    invoice text fix

--- app-route/order/addAppOrder.ts
+++ app-route/order/addAppOrder.ts
@@ -50,13 +50,13 @@ const addAppOrder = async (req: any, res: Response): Promise<Response> => {
       title: "HINT",
       to: address?.email,
       subject: "Order Placed",
-      body: `Thank you for shopping at Mood Studio. Your order has been placed successfully. Order id: ${saved._id}, Total: ${saved.total}. Download your invoice from here: https://mango-frontend-test.vercel.app/my-purchase/${saved?._id}/invoice `,
+      body: `Thank you for shopping at HINT. Your order has been placed successfully. Order id: ${saved._id}, Total: ${saved.total}. Download your invoice from here: https://mango-frontend-test.vercel.app/my-purchase/${saved?._id}/invoice `,
     });
 
     if (address?.phone) {
       sendSMS({
         receiver: address?.phone,
-        message: `Thank you for shopping at Mood Studio. Invoice: ${saved._id}, Tk. ${saved.total}. Download your invoice from here: https://mango-frontend-test.vercel.app/my-purchase/${saved?._id}/invoice . Shop Online: ${process.env.WEBSITE}`,
+        message: `Thank you for shopping at HINT. Invoice: ${saved._id}, Tk. ${saved.total}. Download your invoice from here: https://mango-frontend-test.vercel.app/my-purchase/${saved?._id}/invoice . Shop Online: ${process.env.WEBSITE}`,
       });
     }
 
